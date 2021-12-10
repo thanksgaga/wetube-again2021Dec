df3a7fdc4e670db205359c607e4f2511e84a9a67
thanks.gaga <coach.gayoung@gmail.com>
2021-12-11 00:37:00 +0800

    Join account - form, DB user create, bcrypt password hash

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,17 @@
+import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {};
+export const postJoin = async (req, res) => {
+	console.log(req.body);
+	const { name, username, email, password, location } = req.body;
+	await User.create({
+		name,
+		username,
+		email,
+		password,
+		location,
+	});
+	return res.redirect("/login");
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
