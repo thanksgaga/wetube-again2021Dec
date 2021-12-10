ddbdfba48b66c6df1501885ca18d7ecccadb51fd
thanks.gaga <coach.gayoung@gmail.com>
2021-12-10 23:58:14 +0800

    Join account setup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
-export const join = (req, res) => res.send("Join");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
