b6e04a1e96573d6bbfe33de48d5226bf424165df
thanks.gaga <coach.gayoung@gmail.com>
2021-12-10 19:24:32 +0800

    Udpate after #6.24 statics

---
+++ src/controllers/videoController.js
@@ -0,0 +1,60 @@
+import { render } from "pug";
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", {pageTitle: "Home", videos});
+  }catch(error){
+    return res.end();
+  }
+};
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if(!video){
+    return res.render("404", {pageTitle:"Video not found."});
+  }
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video});
+};
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if(!video){
+    return res.render("404", {pageTitle:"Video not found."});
+  }
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`,video });
+};
+export const postEdit = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({_id:id});
+  if(!video){
+    return res.render("404", {pageTitle:"Video not found."});
+  }
+  await Video.findByIdAndUpdate(id, 
+    {title, description,
+      hashtags: Video.formatHashtags(hashtags), 
+  });
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = async (req, res) => {
+  const {title, description, hashtags} = req.body;
+  console.log(title, description, hashtags);
+  try{
+    await Video.create({
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
+      });
+      return res.redirect("/");
+  }catch(error){
+    console.log(error);
+    res.render("upload", {pageTitle:"Uploading video", errorMessage: error._message});
+  }
+};
---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("💚 Connected to DB");
+const handleError =(error) => console.log("💔 DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
