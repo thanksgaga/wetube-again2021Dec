c8b3abc2abc231a4ae2b490085e2b3e4769a50d7
thanks.gaga <coach.gayoung@gmail.com>
2021-12-11 00:57:36 +0800

    7.4 - Validation, render with status code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,23 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
 	console.log(req.body);
-	const { name, username, email, password, location } = req.body;
+	const { name, username, email, password, password2, location } = req.body;
+	const pageTitle = "Join";
+
+	if (password !== password2) {
+		return res.status(400).render("join", {
+			pageTitle,
+			errorMessage: "Password confirmation doesn't match",
+		});
+	}
+	const exists = await User.exists({ $or: [{ username }, { email }] });
+	if (exists) {
+		return res.status(400).render("join", {
+			pageTitle,
+			errorMessage: "This username/email is already taken",
+		});
+	}
+
 	await User.create({
 		name,
 		username,
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -13,7 +13,7 @@ export const watch = async (req, res) => {
 	const { id } = req.params;
 	const video = await Video.findById(id);
 	if (!video) {
-		return res.render("404", { pageTitle: "Video not found." });
+		return res.status(404).render("404", { pageTitle: "Video not found." });
 	}
 	return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
@@ -38,7 +38,7 @@ export const getEdit = async (req, res) => {
 	const { id } = req.params;
 	const video = await Video.findById(id);
 	if (!video) {
-		return res.render("404", { pageTitle: "Video not found." });
+		return res.status(404).render("404", { pageTitle: "Video not found." });
 	}
 	return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
@@ -47,7 +47,7 @@ export const postEdit = async (req, res) => {
 	const { title, description, hashtags } = req.body;
 	const video = await Video.exists({ _id: id });
 	if (!video) {
-		return res.render("404", { pageTitle: "Video not found." });
+		return res.status(404).render("404", { pageTitle: "Video not found." });
 	}
 	await Video.findByIdAndUpdate(id, {
 		title,
@@ -73,7 +73,7 @@ export const postUpload = async (req, res) => {
 		return res.redirect("/");
 	} catch (error) {
 		console.log(error);
-		res.render("upload", {
+		res.status(400).render("upload", {
 			pageTitle: "Uploading video",
 			errorMessage: error._message,
 		});
