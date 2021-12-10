2b73a4885265d2737ac9163200a8eac15d8861fb
thanks.gaga <coach.gayoung@gmail.com>
2021-12-10 19:41:17 +0800

    Using Video Schema model to find by ID and delete a video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -61,6 +61,6 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const {id} = req.params;
-  console.log( id);
+  await Video.findByIdAndDelete(id);
   return res.redirect("/");
 }
