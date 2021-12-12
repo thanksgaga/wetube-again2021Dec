b6d7c45520cd150f37fd3f9340bf4e4557574373
thanks.gaga <coach.gayoung@gmail.com>
2021-12-12 16:17:49 +0800

    Install connect-mongo and store sessions on mongodb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
+
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -17,8 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
 	session({
 		secret: "Hello",
-		resave: true,
-		saveUninitialized: true,
+		resave: false,
+		saveUninitialized: false,
+		store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
 	}),
 );
 app.use(localsMiddleware);
